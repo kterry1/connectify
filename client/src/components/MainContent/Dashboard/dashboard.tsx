e04c4e7d82cd545e4438421ad34b43e71fd443e0
Kevin Terry <kevin.terry@commercetools.com>
2023-06-11 18:44:59 -0400

    added light/dark switch

--- client/src/components/MainContent/Dashboard/dashboard.tsx
+++ client/src/components/MainContent/Dashboard/dashboard.tsx
@@ -1,28 +1,54 @@
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import * as Label from "@radix-ui/react-label";
-import * as ScrollArea from "@radix-ui/react-scroll-area";
 import "./dashboard.css";
 import { AddButton } from "../../../assets";
 
-const Card = () => {
-  return <div className="Card"></div>;
+const hexToRGBA = (hex: string, opacity: number) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
 };
 
-const CardColumn = () => {
+interface CardProps {
+  colorScheme: string;
+}
+
+const Card = ({ colorScheme }: CardProps) => {
   return (
-    <div className="CardColumn" id="new-card-column">
+    <div
+      className="Card"
+      style={{
+        boxShadow: `5px 10px 20px ${hexToRGBA(colorScheme, 0.15)},
+    3px 6px 6px  ${hexToRGBA(colorScheme, 0.15)}`,
+      }}
+    ></div>
+  );
+};
+
+interface CardColumnProps {
+  label: string;
+  colorScheme: string;
+}
+
+const CardColumn = ({ label, colorScheme }: CardColumnProps) => {
+  return (
+    <div
+      className="CardColumn"
+      id="new-card-column"
+      style={{ color: colorScheme }}
+    >
       <div className="CardColumnHeading">
         <Label.Root className="LabelRoot" htmlFor="new-card-column">
-          New
+          {label}
         </Label.Root>
         <AddButton />
       </div>
       <div className="CardList">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card colorScheme={colorScheme} />
+        <Card colorScheme={colorScheme} />
+        <Card colorScheme={colorScheme} />
       </div>
     </div>
   );
@@ -45,10 +71,10 @@ const Dashboard = () => {
         <button className="ButtonGreen">Refresh Columns</button>
       </div>
       <div className="CardColumnsContainer">
-        <CardColumn />
-        <CardColumn />
-        <CardColumn />
-        <CardColumn />
+        <CardColumn label="New" colorScheme="#9E6375" />
+        <CardColumn label="Open" colorScheme="#FFAB40" />
+        <CardColumn label="In Progress" colorScheme="#7784F9" />
+        <CardColumn label="Done" colorScheme="#D23C63" />
       </div>
     </div>
   );
